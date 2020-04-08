bd3294fa023f627c5c59e7434f3429c5dccbc856
Dmitry <bhcomail@gmail.com>
2020-04-08 17:01:48 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+a
+// var leftBorderWidht = 1;
+// {
+//     let second = 2;
+// }
+
+// const pi = 3.14;
+
+// console.log(second);
+
+
+// console.log(4/0);
+// console.log('string'*9);
+
+// let something;
+// console.log(something);
+
+// let person = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+
+// };
+
+// console.log(person["name"]);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bnp'];
+
+// console.log(arr[2]);
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+isClose = false;
+
+console.log(isChecked || !isClose);
